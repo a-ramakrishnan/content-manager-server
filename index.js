9e960857f39107eb4dd58c47211410e081fcdb93
Ramakrishnan <ramakrishnan.a@pm.me>
2021-09-14 18:43:41 +0530

    Handling data addition

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const fs = require('fs')
 const path = require('path')
 const pathToFile = path.resolve('./data.json')
 
+app.use(express.json())
+
 var corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
@@ -20,6 +22,26 @@ const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 app.get("/api/resources", (req, res) => {
     console.log('Inside the Home page')
     res.status(200).json(getResources())
+})
+
+app.post("/api/resources", (req, res) => {
+    console.log('Inside the Home Post page')
+    const resources = getResources()
+    const resource = req.body
+    resource.createdAt = new Date()
+    resource.id = Date.now().toString()
+
+    resources.unshift(resource)
+
+    fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+        if (error) {
+            return res.status(422).send('Cannot write data to file')
+        }
+        return res.status(200).send("Data added")
+    })
+
+
+
 })
 
 
